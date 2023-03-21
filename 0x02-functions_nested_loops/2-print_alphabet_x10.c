1ec18753d96d2e1947dc2e0e36d8674ac7111f2c
ElRoccinante <alaechouiye@gmail.com>
2023-03-21 16:21:41 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,18 +7,18 @@
 
 void print_alphabet_x10(void)
 {
-    char letter;
-    int i = 0;
+	char letter;
+	int i = 0;
 
-    while (i < 10)
-    {
-        letter = 'a';
-        while (letter <= 'z')
-        {
-            _putchar(letter);
-            letter++;
-        }
-        _putchar('\n');
-        i++;
-    }
+	while (i < 10)
+	{
+	letter = 'a';
+	while (letter <= 'z')
+	{
+	 _putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+	i++;
+	}
 }
