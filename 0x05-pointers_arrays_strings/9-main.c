e8cf3001f906abde37b500a92754b0d444044b63
ElRoccinante <alaechouiye@gmail.com>
2023-03-26 22:19:30 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-main.c
+++ 0x05-pointers_arrays_strings/9-main.c
@@ -1,18 +1,20 @@
-#include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * print_array - a function that prints n elements of an array
+ * @a: array name
+ * @n: is the number of elements OF the array to be printed
+ * Return: a and n inputs
  */
-int main(void)
+void print_array(int *a, int n)
 {
-  char s1[98];
-  char *p;
+	int i;
 
-  p = _strcpy(s1, "First, solve the problem. Then, write the code\n");
-  printf("%s", s1);
-  printf("%s", p);
-  return (0);
+	for (i = 0; i < (n - 1); i++)
+	{
+		printf("%d, ", a[i]);
+	}
+		if (i == (n - 1))
+		{
+			printf("%d", a[n - 1]);
+		}
+			printf("\n");
 }
