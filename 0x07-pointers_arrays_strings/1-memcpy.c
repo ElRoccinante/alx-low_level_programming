5a686e8adfde40c391ee31ba96929e0c0ef2c469
ElRoccinante <alaechouiye@gmail.com>
2023-04-04 06:14:06 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,14 +1,11 @@
 #include "main.h"
 
 /**
- * _memcpy - copies a block of memory from a source to a destination
- * @dest: pointer to the destination buffer where data is to be copied
- * @src: pointer to the source of data to be copied
- * @n: number of bytes to be copied
- * 
- * This function copies the values of n bytes from the location pointed by 'src'
- * to the memory block pointed by 'dest'. If the memory blocks overlap, the behavior
- * is undefined. The function returns a pointer to the destination buffer 'dest'.
+ * _memcpy - a function that copys memory area
+ * @dest: memory area destination to copy
+ * @src: memory area source to copy from
+ * @n: number of bytes
+ * Return: returns pointer to copy location
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
