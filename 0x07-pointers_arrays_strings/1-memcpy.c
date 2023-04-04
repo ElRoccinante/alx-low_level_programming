9d0976916b984274369b9849a4c90ec81e5073bd
ElRoccinante <alaechouiye@gmail.com>
2023-04-04 05:28:32 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,25 +1,60 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _memcpy - Copies a specified number of bytes from one memory address to another
+ * simple_print_buffer - prints buffer in hexadecimal format
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
  *
- * @dest: Pointer to the destination memory area
- * @src: Pointer to the source memory area
- * @n: Number of bytes to copy
- *
- * Return: Pointer to the destination memory area
+ * This function prints a buffer in hexadecimal format.
  */
-char *_memcpy(char *dest, char *src, unsigned int n)
+void simple_print_buffer(char *buffer, unsigned int size)
 {
-	int i = n;
+  unsigned int i;
 
-	/* Copy each byte from source to destination */
-	for (int r = 0; r < i; r++)
-	{
-		dest[r] = src[r];
-		n--;
-	}
+  i = 0;
+  while (i < size)
+    {
+      /* Print a space after every 10 bytes */
+      if (i % 10)
+        {
+	  printf(" ");
+        }
+      /* Print a new line after every 10 groups of 10 bytes */
+      if (!(i % 10) && i)
+        {
+	  printf("\n");
+        }
+      /* Print each byte in hexadecimal format */
+      printf("0x%02x", buffer[i]);
+      i++;
+    }
+  printf("\n");
+}
 
-	/* Return a pointer to the destination memory area */
-	return (dest);
+/**
+ * main - Entry point
+ *
+ * This is the main function that tests the _memcpy function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  char buffer[98] = {0};
+  char buffer2[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+
+  /* Print the contents of the empty buffer */
+  printf("Buffer contents before memcpy:\n");
+  simple_print_buffer(buffer, 98);
+
+  /* Copy 10 bytes from buffer2 to buffer starting at position 50 */
+  _memcpy(buffer + 50, buffer2, 10);
+
+  /* Print the contents of the updated buffer */
+  printf("-------------------------------------------------\n");
+  printf("Buffer contents after memcpy:\n");
+  simple_print_buffer(buffer, 98);    
+
+  return (0);
 }
