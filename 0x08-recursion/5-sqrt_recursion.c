f79178e200ab7b98dd1c721f4e59c0214066ecd3
ElRoccinante <alaechouiye@gmail.com>
2023-04-06 08:35:16 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,37 +1,20 @@
-#include "main.h"
-
 /**
- * _sqrt_recursion - Calcule la racine carrée de n.
- * @n: Un entier en entrée.
- * Return: La racine carrée de n.
+ * _sqrt_iterative - It returns the value of square root of n.
+ * @n: an input integer
+ * Return: The square root of n
  */
-int _sqrt_recursion(int n)
+int _sqrt_iterative(int n)
 {
 	if (n < 0)
 		return (-1);
 
-	return (_sqrt_helper(n, 0, n));
-}
+	int i = 1;
+	while (i * i <= n)
+	{
+		if (i * i == n)
+			return (i);
+		i++;
+	}
 
-/**
- * _sqrt_helper - Calcule la racine carrée de n en utilisant la recherche binaire.
- * @n: La valeur dont on cherche la racine carrée.
- * @start: La valeur de départ pour la recherche binaire.
- * @end: La valeur de fin pour la recherche binaire.
- * Return: La racine carrée de n.
- */
-int _sqrt_helper(int n, int start, int end)
-{
-	if (start > end)
-		return (-1);
-
-	int mid = start + (end - start) / 2;
-	int square = mid * mid;
-
-	if (square == n)
-		return (mid);
-	else if (square > n)
-		return (_sqrt_helper(n, start, mid - 1));
-	else
-		return (_sqrt_helper(n, mid + 1, end));
+	return (-1); // Si n n'est pas un carré parfait, retourne -1
 }
