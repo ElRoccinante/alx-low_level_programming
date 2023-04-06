727050c39b40734a0764e511e84fc39f3fcac386
ElRoccinante <alaechouiye@gmail.com>
2023-04-06 09:22:30 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,20 +1,19 @@
 #include "main.h"
-
 /**
  * wildcmp - Une fonction qui compare deux chaînes de caractères
- * @s1: Premier argument chaîne de caractères
- * @s2: Deuxième argument chaîne de caractères
+ * @s1: Premier input string
+ * @s2: Deuxième input string
  * Return: 1 si les chaînes sont identiques, 0 sinon
  */
 int wildcmp(char *s1, char *s2)
 {
-    if (!*s1 && !*s2)
-        return (1);
-    if (*s1 == *s2)
-        return (wildcmp(s1 + 1, s2 + 1));
-    if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
-        return (1);
-    if (*s2 == '*' && *(s1 + 1) && *s2)
-        return (0);
-    return (0);
+	if (!*s1 && !*s2)
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
+		return (1);
+	if (*s2 == '*' && *(s1 + 1) && *s2)
+		return (0);
+	return (0);
 }
