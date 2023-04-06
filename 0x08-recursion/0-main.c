505d26647e3bd6c6a9ccc2ffa90a622e53438a6b
ElRoccinante <alaechouiye@gmail.com>
2023-04-06 06:54:18 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program.
+ *
+ * Description: This program demonstrates the usage of the _puts_recursion
+ *              function to print a string recursively.
+ *
+ * Return: Always 0 (indicating success).
+ */
+int main(void)
+{
+	_puts_recursion("Betty Holberton"); /* Call _puts_recursion function with input string */
+	return (0); /* Return 0 to indicate successful termination of the program */
+}
---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string recursively, followed by a newline.
+ * @s: Pointer to the input string to be printed.
+ *
+ * Return: None
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s); /* Print the current character */
+		_puts_recursion(s + 1); /* Call the function recursively with the next character */
+	}
+	else
+	{
+		_putchar('\n'); /* Print a newline when end of string is reached */
+	}
+}
