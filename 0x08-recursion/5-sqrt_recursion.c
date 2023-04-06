cc4ddf53e5855ed782139913294142a159f7042b
ElRoccinante <alaechouiye@gmail.com>
2023-04-06 08:20:52 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,26 +1,63 @@
-#include "main.h"
-
-int _sqrt(int prev, int root); // Declaration of _sqrt function
+#include <stdio.h>
 
 /**
- * _sqrt_recursion - Calculates the square root of n using recursion.
- * @n: An input integer.
- * Return: The square root of n, or -1 if n is negative.
+ * _sqrt_recursion - Calculates the natural square root of a number.
+ * @n: The number to calculate the square root of.
+ *
+ * Return: The natural square root of n, or -1 if n does not have a natural square root.
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
+	if (n < 0) /* Check for invalid input */
+		return -1;
+	if (n == 0 || n == 1) /* Base case: square root of 0 and 1 is itself */
+		return n;
 
-	return (_sqrt(1, n));
+	return _sqrt_helper(n, 1, n); /* Call helper function to calculate square root */
 }
 
-
-int _sqrt(int prev, int root)
+/**
+ * _sqrt_helper - Helper function to calculate square root recursively.
+ * @n: The number to calculate the square root of.
+ * @start: The starting value for searching the square root.
+ * @end: The ending value for searching the square root.
+ *
+ * Return: The natural square root of n, or -1 if n does not have a natural square root.
+ */
+int _sqrt_helper(int n, int start, int end)
 {
-	if (prev > root)
-		return (-1);
-	else if (prev * prev == root)
-		return (prev);
-	return (_sqrt(prev + 1, root));
+	if (start <= end)
+	{
+		int mid = start + (end - start) / 2;
+		int square = mid * mid;
+
+		if (square == n)
+			return mid;
+		else if (square < n)
+			return _sqrt_helper(n, mid + 1, end);
+		else
+			return _sqrt_helper(n, start, mid - 1);
+	}
+
+	return -1; /* Return -1 if n does not have a natural square root */
+}
+
+int main(void)
+{
+	int r;
+
+	r = _sqrt_recursion(1);
+	printf("%d\n", r); /* Expected output: 1 */
+	r = _sqrt_recursion(1024);
+	printf("%d\n", r); /* Expected output: 32 */
+	r = _sqrt_recursion(16);
+	printf("%d\n", r); /* Expected output: 4 */
+	r = _sqrt_recursion(17);
+	printf("%d\n", r); /* Expected output: -1 */
+	r = _sqrt_recursion(25);
+	printf("%d\n", r); /* Expected output: 5 */
+	r = _sqrt_recursion(-1);
+	printf("%d\n", r); /* Expected output: -1 */
+
+	return 0;
 }
