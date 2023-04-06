e8052c6a32d2a7b3d16dbbd700f6f8fb69a6ef17
ElRoccinante <alaechouiye@gmail.com>
2023-04-06 09:03:27 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,28 +1,43 @@
-/*                                                                                                                                    
- * File: 6-is_prime_number.c                                                                                                          
- * Auth: sam tech                                                                                                                     
- */                                                                                                                                   
-                                                                                                                                      
-#include "main.h"                                                                                                                     
-                                                                                                                                      
-int is_divisible(int num, int div);                                                                                                   
-int is_prime_number(int n);                                                                                                           
-                                                                                                                                      
-/**                                                                                                                                   
- * is_divisible - Checks if a number is divisible.                                                                                    
- * @num: The number to be checked.                                                                                                    
- * @div: The divisor.                                                                                                                 
- *                                                                                                                                    
- * Return: If the number is divisible - 0.                                                                                            
- *         If the number is not divisible - 1.                                                                                        
- */                                                                                                                                   
-int is_divisible(int num, int div)                                                                                                    
-{                                                                                                                                     
-        if (num % div == 0)                                                                                                           
-                return (0);                                                                                                           
-                                                                                                                                      
-        if (div == num / 2)                                                                                                           
-                return (1);                                                                                                           
-                                                                                                                                      
-        return (is_divisible(num, div + 1));                                                                                          
+/*
+ * File: 6-is_prime_number.c
+ * Auth: sam tech
+ */
+
+#include "main.h"
+
+int is_prime_number(int n);
+int is_divisible(int num, int div);
+
+/**
+ * is_prime_number - Checks if a number is a prime number.
+ * @n: The number to be checked.
+ *
+ * Return: If the number is a prime number - 1.
+ *         If the number is not a prime number - 0.
+ */
+int is_prime_number(int n)
+{
+        if (n <= 1)
+                return (0);
+
+        return (is_divisible(n, 2));
+}
+
+/**
+ * is_divisible - Checks if a number is divisible.
+ * @num: The number to be checked.
+ * @div: The divisor.
+ *
+ * Return: If the number is divisible - 0.
+ *         If the number is not divisible - 1.
+ */
+int is_divisible(int num, int div)
+{
+        if (div >= num / 2)
+                return (1);
+
+        if (num % div == 0)
+                return (0);
+
+        return (is_divisible(num, div + 1));
 }
