0619d6b8f9dd5bc595daa37b71ab7de19ff50f3c
ElRoccinante <alaechouiye@gmail.com>
2023-04-09 13:46:52 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,18 +6,21 @@
  * @argv: An array of pointers to the arguments
  *
  * Description: This program prints the names of the arguments passed to it,
- *              separated by spaces, followed by a new line.
+ * separated by spaces, followed by a new line.
  *
  * Return: Always returns 0 to indicate successful execution
  */
 
 int main(int argc, char **argv)
 {
-    int i;
-    for (i = 0; i < argc; i++)
-    {
-        printf("%s", argv[i]);
-    }
-    printf("\n");
-    return (0);
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s", argv[i];
+	}
+
+	printf("\n");
+
+	return (0);
 }
