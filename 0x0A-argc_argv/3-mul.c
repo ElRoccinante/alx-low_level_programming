60872153e9ab1a2da59441fe79842abb78366367
ElRoccinante <alaechouiye@gmail.com>
2023-04-09 14:05:08 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * main - program
+ * @argc: argcountedddddr
+ * @argv: argvalues
+ * Return: Always 0 jkkk
+ */
 int main(int argc, char *argv[])
 {
-	int y1, y2;
-
-	if (argc != 3)
-	{
-		printf("Error\n");
-		return 1;
-	}
-
-	y1 = atoi(argv[1]);
-	y2 = atoi(argv[2]);
-	printf("%d\n", (argc == 3) ? y1 * y2 : 0);
-
-	return 0;
+int y1;
+int y2;
+int i;
+for (i = 0; i < argc; i++)
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+else
+y1 = atoi(argv[1]);
+y2 = atoi(argv[2]);
+printf("%d\n", y1 *y2);
+return (0);
 }
