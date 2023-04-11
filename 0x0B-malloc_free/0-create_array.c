29a81176f406e06104052307e76b72c4613a2f28
ElRoccinante <alaechouiye@gmail.com>
2023-04-11 10:04:20 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,18 +10,24 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	
+	// Allocate memory for the array
 	char *s = (char *)malloc(size * sizeof(char));
 
-	
+	// Check if memory allocation fails
 	if (s == NULL)
 		return NULL;
 
+	// Check if size is 0 and return NULL
 	if (size == 0)
 		return NULL;
 
-	for (unsigned int i = 0; i < size; i++)
+	// Declare the loop counter variable
+	unsigned int i;
+
+	// Initialize the array with the specified character
+	for (i = 0; i < size; i++)
 		s[i] = c;
 
+	// Return the pointer to the array
 	return s;
 }
