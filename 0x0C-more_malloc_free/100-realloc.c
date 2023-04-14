9a9757e4b5c45337e8d5c83b47547afb6e6e8169
ElRoccinante <alaechouiye@gmail.com>
2023-04-14 11:50:30 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -40,7 +40,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		old_size = new_size;
 
 	for (unsigned int i = 0; i < old_size; i++)
+	{
 		((char *)new_ptr)[i] = ((char *)ptr)[i];
+	}
 
 	free(ptr);
 	return (new_ptr);
