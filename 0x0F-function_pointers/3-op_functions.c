6fdbec5a176427fb46d00747501de7af2d6c1c33
ElRoccinante <alaechouiye@gmail.com>
2023-04-19 17:38:09 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -49,7 +49,6 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
-if (b == 0)
 	return (a / b);
 }
 /**
@@ -61,7 +60,6 @@ if (b == 0)
  */
 int op_mod(int a, int b)
 {
-if (b == 0)
 	return (a % b);
 }
 
