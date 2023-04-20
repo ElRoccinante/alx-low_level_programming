b5c88597bc04447187beefcf9101300c3dfc2222
ElRoccinante <alaechouiye@gmail.com>
2023-04-19 18:02:23 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,72 +2,60 @@
 
 /**
  * op_add - Returns the sum of two numbers.
- * @a: The first number.
- * @b: The second number.
+ * @a: The first operand.
+ * @b: The second operand.
  *
  * Return: The sum of a and b.
  */
 int op_add(int a, int b)
 {
-	return (a + b);
+    return (a + b);
 }
 
 /**
  * op_sub - Returns the difference of two numbers.
- * @a: The first number.
- * @b: The second number.
+ * @a: The first operand.
+ * @b: The second operand.
  *
  * Return: The difference of a and b.
  */
 int op_sub(int a, int b)
 {
-	return (a - b);
+    return (a - b);
 }
 
 /**
  * op_mul - Returns the product of two numbers.
- * @a: The first number.
- * @b: The second number.
+ * @a: The first operand.
+ * @b: The second operand.
  *
  * Return: The product of a and b.
  */
 int op_mul(int a, int b)
 {
-	return (a * b);
+    return (a * b);
 }
 
 /**
- * op_div - Returns the division of two numbers.
- * @a: The first number.
- * @b: The second number.
+ * op_div - Returns the quotient of two numbers.
+ * @a: The first operand.
+ * @b: The second operand.
  *
  * Return: The quotient of a and b.
- * 
- * Note: If b is 0, this function returns 0.
  */
 int op_div(int a, int b)
 {
-	if (b == 0) {
-		printf("Error: Cannot divide by zero\n");
-		return 0;
-	}
-	return (a / b);
+    return (a / b);
 }
 
 /**
  * op_mod - Returns the remainder of the division of two numbers.
- * @a: The first number.
- * @b: The second number.
+ * @a: The first operand.
+ * @b: The second operand.
  *
  * Return: The remainder of the division of a by b.
- * 
- * Note: If b is 0, this function returns 0.
  */
 int op_mod(int a, int b)
 {
-	if (b == 0) {
-		printf("Error: Cannot modulo by zero\n");
-		return 0;
-	}
-	return (a % b);
+    return (a % b);
 }
