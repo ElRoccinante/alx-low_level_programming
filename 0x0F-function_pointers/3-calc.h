6ef056189894940de67c40a2808ff9cfd6aef555
ElRoccinante <alaechouiye@gmail.com>
2023-04-19 18:58:31 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -2,24 +2,21 @@
 #define CALC_H
 
 /**
- * struct op - A struct to store an operator and its associated function.
- * @op: The operator as a string.
+ * struct op - A struct op.
+ * @op: The operator.
  * @f: The associated function.
  */
 typedef struct op
 {
-	char *op;
-	int (*f)(int a, int b);
+    char *op;
+    int (*f)(int a, int b);
 } op_t;
 
-/* Function prototypes for the various operations */
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-
-/* Function prototype for getting the corresponding operation function */
 int (*get_op_func(char *s))(int, int);
 
-#endif
+#endif /* CALC_H */ 
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,9 +1,32 @@
+#ifndef CALC_H
+#define CALC_H
+
+/**
+ * struct op - A struct op.
+ * @op: The operator.
+ * @f: The associated function.
+ */
+typedef struct op
+{
+    char *op;
+    int (*f)(int a, int b);
+} op_t;
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+
+#endif /* CALC_H */ 
+
 #include "3-calc.h"
 #include <stdlib.h>
 
 /**
- * get_op_func - selects the correct function to perform the operation
- *               asked by the user.
+ * get_op_func - Selects the correct function to perform
+ *               the operation asked by the user.
  * @s: The operator passed as argument.
  *
  * Return: A pointer to the function corresponding
@@ -17,7 +40,7 @@ int (*get_op_func(char *s))(int, int)
         {"*", op_mul},
         {"/", op_div},
         {"%", op_mod},
-        {NULL, NULL}
+        {NULL, NULL},
     };
 
     int i = 0;
