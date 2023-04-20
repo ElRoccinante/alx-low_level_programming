154024356e506bb8491ceaf86b96af1e5c6ddcb0
ElRoccinante <alaechouiye@gmail.com>
2023-04-19 17:01:47 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * get_op_func - Selects the correct function to perform
+ *               the operation asked by the user.
+ * @s: The operator passed as argument.
+ *
+ * Return: A pointer to the function corresponding
+ *         to the operator given as a parameter.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	/* Declare the struct containing the operators and their respective functions */
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i = 0;
+
+	/* Iterate through the struct until the correct operator is found */
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
+
+	/* Return the corresponding function */
+	return (ops[i].f);
+}
