274044499ce0751870574bb7394bdbe5774e456a
ElRoccinante <alaechouiye@gmail.com>
2023-04-19 19:26:39 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -2,25 +2,21 @@
 #define CALC_H
 
 /**
- * struct op - Struct for operators
+ * struct op - Struct op
  *
- * @op: The operator symbol
- * @f: The function associated with the operator
+ * @op: The operator
+ * @f: The function associated
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
-/* Function prototypes for basic arithmetic operations */
-int op_add(int a, int b); /* Adds two integers */
-int op_sub(int a, int b); /* Subtracts two integers */
-int op_mul(int a, int b); /* Multiplies two integers */
-int op_div(int a, int b); /* Divides two integers */
-int op_mod(int a, int b); /* Computes modulus of two integers */
-
-/* Function prototype for a function that selects the correct operation function based on the operator passed as argument */
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
-
 #endif
