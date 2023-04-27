0663ba54a27d0ecfa910e7740693759f41eefced
ElRoccinante <alaechouiye@gmail.com>
2023-04-27 11:30:54 -0700

    all task i hope

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,15 +11,15 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node = malloc(sizeof(list_t));
+list_t *new_node = malloc(sizeof(list_t));
 
-    if (new_node == NULL)
-        return (NULL);
+	if (new_node == NULL)
+	return (NULL);
 
-    new_node->str = strdup(str);
-    new_node->len = strlen(str);
-    new_node->next = *head;
-    *head = new_node;
+new_node->str = strdup(str);
+new_node->len = strlen(str);
+new_node->next = *head;
+*head = new_node;
 
-    return new_node;
+	return (new_node);
 }
