b1afd779d278cc2094af94830673626e0e8d06dc
ElRoccinante <alaechouiye@gmail.com>
2023-05-02 08:27:21 -0700

    second-commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,9 +1,11 @@
 #include "lists.h"
+
 /**
-*This function prints all the elements of a singly linked list of integers.
-*@h: A pointer to the head node of the list.
-*@return: The number of nodes in the list.
-*/
+ * print_listint - Prints all the elements of a singly linked list of integers
+ * @h: A pointer to the head node of the list
+ *
+ * Return: The number of nodes in the list
+ */
 size_t print_listint(const listint_t *h)
 {
 	size_t cmp = 0;
