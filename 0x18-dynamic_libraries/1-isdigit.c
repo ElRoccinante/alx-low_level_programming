743f7be3fde2228f62cda38b2c8fe0d3fadbeb71
Alae Chouiyakh <alaechouiye@gmail.com>
2023-06-19 14:37:56 -0700

    test

---
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+* _isdigit - checks a digit
+* @c: check a digit
+* Return: 1 if digit, in other case 0
+*/
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+
+	return (0);
+}
