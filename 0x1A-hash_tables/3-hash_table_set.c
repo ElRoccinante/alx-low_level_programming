dca6e36e769aee18bc139936642520b10a9d54a1
Alae Chouiyakh <alaechouiye@gmail.com>
2023-07-07 05:15:20 -0700

    function_that_add_element_to_hashtanm

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -31,15 +31,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 }
 	tmp = tmp->next;
 }
-
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
-        return (0);
+	return (0);
 
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
-    return (1);
+	return (1);
 }
