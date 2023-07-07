f0a1c5ccc19077be76e921990bb58a472a22e666
Alae Chouiyakh <alaechouiye@gmail.com>
2023-07-07 06:48:08 -0700

    wow

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -45,11 +45,11 @@ void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 
 /**
- * struct shash_node_s
- * @key: The key
+ * struct shash_node_s - Node of a sorted hash table
+ * @key: The key, string
  * The key is unique in the HashTable
- * @value: value corresponding to a key
- * @next: A pointer to the next node
+ * @value: The value corresponding to a key
+ * @next: A pointer to the next node of the List
  * @sprev: A pointer to the previous element of the sorted linked list
  * @snext: A pointer to the next element of the sorted linked list
  */
