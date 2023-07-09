906f2537358fad4b752b4dabc675d6350843349e
Alae Chouiyakh <alaechouiye@gmail.com>
2023-07-09 04:33:49 -0700

    j

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -72,7 +72,14 @@ typedef struct shash_node_s
  * @stail: A pointer to the last element of the sorted linked list
  */
 
-
+shash_table_t *shash_table_create(unsigned long int size);
+shash_node_t *add_n_shash(shash_node_t **, const char *, const char *);
+void add_i_shash(shash_table_t *ht, shash_node_t *new);
+int shash_table_set(shash_table_t *ht, const char *key, const char *value);
+char *shash_table_get(const shash_table_t *ht, const char *key);
+void shash_table_print(const shash_table_t *ht);
+void shash_table_print_rev(const shash_table_t *ht);
+void shash_table_delete(shash_table_t *ht);
 
 
 
