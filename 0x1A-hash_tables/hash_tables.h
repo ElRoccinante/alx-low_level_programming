d847c5b19c476f0e03f2f2afc63d05b1a698d59e
ElRoccinante <alaechouiye@gmail.com>
2023-09-08 00:47:31 -0700

    h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -32,5 +32,8 @@ hash_node_t **array;
 
 /* Function prototypes */
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+
+
 
 #endif /* HASH_TABLES_H */
