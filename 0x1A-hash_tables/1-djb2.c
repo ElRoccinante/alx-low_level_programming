984c4429a9cf7131ce0dda9b89befd9ae31619ef
ElRoccinante <alaechouiye@gmail.com>
2023-09-08 00:53:14 -0700

    h

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,12 +1,18 @@
+/**
+ * hash_djb2 - Implements the djb2 hash algorithm.
+ * @str: The string to be hashed.
+ *
+ * Return: The hash value for the given string.
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-unsigned long int hash;
-int c;
+	unsigned long int hash;
+	int c;
 
-hash = 5381;
-while ((c = *str++))
-{
-hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-}
-return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+	hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
