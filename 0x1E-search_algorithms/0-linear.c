5cc5d8a8f6518a0710fc04235d26c6be3ed1ef97
ElRoccinante <alaechouiye@example.com>
2023-12-13 06:10:16 -0800

    searches for a value in an array of integers

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -4,7 +4,6 @@
  * @array: Pointer to the first element of the array to search in.
  * @size: Number of elements in the array.
  * @value: Value to search for.
- *
  * Return: The first index where value is located, or -1 if not found.
  */
 int linear_search(int *array, size_t size, int value)
